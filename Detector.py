3a86e475d6ffb433e0da5cfda0aafd84ac400501
mehul009 <31311720+mehul009@users.noreply.github.com>
2017-12-27 16:54:11 +0530

    Update Detector.py

--- Detector.py
+++ Detector.py
@@ -2,15 +2,16 @@ from __future__ import unicode_literals
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from matplotlib.pyplot import psd
-from PyQt5 import QtCore, QtWidgets
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
-import matplotlib
+
 import matplotlib.pyplot as plt
 import sys
 import rtlsdr
 import peakutils
+import pandas as pd
+import time
+import os
 
 port = rtlsdr.RtlSdr()              # configuration of devise
 sr = 2.4                                     # sample rate in mega Hz
@@ -24,11 +25,15 @@ port.gain = 'auto'
 
 num = 5      # 2**num work as overlapping constant
 
+data = port.read_samples(1024 * 2 ** num)   # data
+
 start = 0	 # Zooming constant
 end = 6
 div = 1024/6
 
 coordinate = 0   # coordinate constant
+
+save = 'no'  # save constant
 
 
 class MyMplCanvas(FigureCanvas):         # made a canvas
@@ -43,7 +48,7 @@ class MyMplCanvas(FigureCanvas):         # made a canvas
         FigureCanvas.__init__(self, fig)
         self.setParent(parent)
 
-        FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
         FigureCanvas.updateGeometry(self)
 
     def compute_initial_figure(self):
@@ -54,21 +59,21 @@ class MyDynamicMplCanvas(MyMplCanvas):                # canvas for graph
 
     def __init__(self, *args, **kwargs):
         MyMplCanvas.__init__(self, *args, **kwargs)
-        timer = QtCore.QTimer(self)                    # timer for dynamic graph
+        timer = QTimer(self)                    # timer for dynamic graph
         timer.timeout.connect(self.update_figure)
-        timer.start(2)
+        timer.start(200)
 
     def compute_initial_figure(self):
         data = port.read_samples(1024 * 2 ** num)
 
-    def update_figure(self):                            # dynamic graph
+    def update_figure(self):   # dynamic graph
         global data
         data = port.read_samples(1024 * 2 ** num)
         global x
         global y
-        y, x = matplotlib.pyplot.psd(data, NFFT=1024, Fs=sr, Fc=k)               # psd of incoming data
+        y, x = plt.psd(data, NFFT=1024, Fs=sr, Fc=k)               # psd of incoming data
         self.axes.cla()
-        self.axes.set_xlabel('Central Frequency = '+str('%.3f' % k)+' MHz')
+        self.axes.set_xlabel('Central Frequency = ' + str('%.3f' % k) + ' MHz')
         self.axes.set_ylabel('Power  Spectrum Value')
         self.axes.plot(x[int(start*div):int(end*div)], y[int(start*div):int(end*div)], 'r')        # plot the data
         ind = peakutils.indexes(y[int(start*div):int(end*div)], thres=0.5, min_dist=30)       # find the peak
@@ -82,6 +87,31 @@ class MyDynamicMplCanvas(MyMplCanvas):                # canvas for graph
             xx.append(x1[aa])
             yy.append(y1[aa])
         self.axes.plot(xx, yy, '*')  # plot a pick
+
+        if save == 'yes':
+            year, month, day, hour, min, sec, wday, yday, isdst = time.localtime()  # time for save file
+
+        # if not exist than make directory path for save file
+            if not os.path.exists(
+                '/home/mehul09/astronomy/' + 'year ' + str(year) + '/' + 'month ' + str(month) + '/' + 'day '
+                                           + str(day) + '/' + 'hour ' + str(hour) + '/'):
+                os.makedirs('/home/mehul09/astronomy/' + 'year ' + str(year) + '/' + 'month ' + str(month) + '/'
+                            + 'day ' + str(day) + '/' + 'hour ' + str(hour) + '/')
+
+            raws = pd.Series(data)  # make a series
+            ssr = pd.Series([sr])
+            scf = pd.Series([k])
+            ssam = pd.Series([1024 * 2 ** num])
+            second = pd.Series([sec])
+
+        # make a frame
+            frame = {'data': raws, 'samplerate': ssr, 'centralfreq': scf, 'sample': ssam, 'second': second}
+            data_frame = pd.DataFrame(frame)  # make data frame
+
+        # save file in h5 formate for highspeed and high compression
+            data_frame.to_hdf('/home/mehul09/astronomy/' + 'year ' + str(year) + '/' + 'month ' + str(month) + '/' +
+                              'day ' + str(day) + '/' + 'hour ' + str(hour) + '/' + 'minute ' + str(min) + '.h5',
+                              key='int', mode='a', formate='table(t)', append=True, complevel=1, fletcher32=True)
 
         def onclick(event):
             global ty
@@ -105,22 +135,27 @@ class MyDynamicMplCanvas(MyMplCanvas):                # canvas for graph
         self.draw()
 
 
-class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui window
+class ApplicationWindow(QMainWindow):                 # main gui window
     def __init__(self):
-        QtWidgets.QMainWindow.__init__(self)
+        QMainWindow.__init__(self)
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.changePosition)
         self.timer.start(1)
 
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+        self.setAttribute(Qt.WA_DeleteOnClose)
         self.setWindowTitle("RADIO ASTRONOMY PROJECT")
 
-        self.file_menu = QtWidgets.QMenu('&File', self)         # make a menu bar
-        self.file_menu.addAction('&Save', self.savefig)
-        self.file_menu.addAction('&Quit', self.fileQuit, QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+        self.file_menu = QMenu('&File', self)         # make a menu bar
+        self.file_menu.addAction('&Screen Shot', self.savefig, Qt.CTRL + Qt.Key_S)
+        savedt = QAction('Save Data', self, checkable=True)
+        savedt.setStatusTip('Save Data')
+        savedt.setChecked(False)
+        savedt.triggered.connect(self.savedata)
+        self.file_menu.addAction(savedt)
+        self.file_menu.addAction('&Quit', self.fileQuit, Qt.CTRL + Qt.Key_Q)
         self.menuBar().addMenu(self.file_menu)
 
-        self.viewMenu = QtWidgets.QMenu('&View', self)
+        self.viewMenu = QMenu('&View', self)
         viewStatAct = QAction('Zoom', self, checkable=True)
         viewStatAct.setStatusTip('Zoom')
         viewStatAct.setChecked(False)
@@ -128,12 +163,12 @@ class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui windo
         self.viewMenu.addAction(viewStatAct)
         self.menuBar().addMenu(self.viewMenu)
 
-        self.editmenu = QtWidgets.QMenu('&Edit', self)
+        self.editmenu = QMenu('&Edit', self)
         self.editmenu.addAction('&Sample Rate', self.ChangeSampleRate)
-        self.editmenu.addAction('&Over Lap',self.OverLap)
+        self.editmenu.addAction('&Over Lap', self.OverLap)
         self.menuBar().addMenu(self.editmenu)
 
-        self.help_menu = QtWidgets.QMenu('&Help', self)
+        self.help_menu = QMenu('&Help', self)
         self.menuBar().addSeparator()
         self.menuBar().addMenu(self.help_menu)
         self.help_menu.addAction('&Zoom', self.infozoom)
@@ -158,17 +193,18 @@ class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui windo
         sld.setGeometry(10, 650, 1500, 30)
         sld.valueChanged.connect(self.scroll)
         self.pos = QLabel('Position')
-        self.shortcut = QShortcut(QKeySequence("Enter"), self)
+
+        self.shortcut = QShortcut(QKeySequence("Enter"), self)      # connect enter btn to input tab
         self.shortcut.activated.connect(self.on_open)
 
-        self.main_widget = QtWidgets.QWidget(self)
+        self.main_widget = QWidget(self)
 
-        self.main_Vlayout = QtWidgets.QVBoxLayout(self.main_widget)  # put widget
-        sub1_Hlayout = QtWidgets.QHBoxLayout(self.main_widget)
+        self.main_Vlayout = QVBoxLayout(self.main_widget)  # put widget
+        sub1_Hlayout = QHBoxLayout(self.main_widget)
         sub1_Hlayout.addWidget(self.cent_freqEdit)
         sub1_Hlayout.addWidget(ebtn)
 
-        self.sub2_Hlayout = QtWidgets.QHBoxLayout(self.main_widget)
+        self.sub2_Hlayout = QHBoxLayout(self.main_widget)
         self.sub2_Hlayout.addWidget(self.slbl)
         self.sub2_Hlayout.addWidget(self.zstart)
         self.sub2_Hlayout.addWidget(self.elbl)
@@ -208,7 +244,6 @@ class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui windo
             self.zend.hide()
             self.reset.hide()
             self.zbtn.hide()
-
 
     def scroll(self, value):
         global k
@@ -269,9 +304,18 @@ class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui windo
         cf = h
         k = cf
 
+    def savedata(self, state):
+        global save
+        if state:
+            save = 'yes'
+        else:
+            save = 'no'
+
     def savefig(self):
         plt.figure(1)
         plt.cla()
+        plt.xlabel('Central Frequency = '+str('%.3f' % k)+' MHz')
+        plt.ylabel('Power Spectrum Value')
         plt.plot(x[int(start*div):int(end*div)], y[int(start*div):int(end*div)], 'r')
         xt, zz = plt.xticks()
         yt, zz = plt.yticks()
@@ -279,14 +323,14 @@ class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui windo
         ln = len(xx)
         sf = 0
         txt = 'peak X:Y'
-        while sf < ln :
-            xx1=xx[sf]
-            yy1=yy[sf]
+        while sf < ln:
+            xx1 = xx[sf]
+            yy1 = yy[sf]
             ax = str('%.2f' % xx1) + ' : ' + str('%.5f' % yy1)
             txt = txt + '\n' + str(ax)
             sf = sf+1
-        plt.text(xt[-2], yt[-2], txt, fontsize=10)
-        plt.savefig('psd of central frequency ' + str(k) + '.png')
+        plt.text(xt[-3], yt[-3], txt, fontsize=8)
+        plt.savefig('psd of central frequency "' + str(k) + '" MHz.svg')
         plt.close()
 
     def fileQuit(self):
@@ -304,12 +348,12 @@ class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui windo
 
     def OverLap(self):
         j, okPressed = QInputDialog.getDouble(self, "Select Over Lap", "Select Number :", 5)
-        if okPressed :
+        if okPressed:
             global num
-            num=j
+            num = j
 
     def about(self):                     # information of software
-        QtWidgets.QMessageBox.about(self, "About", """     Welcome to Simple Rtl-Sdr Software Made by <u><b>Mehul Sutariya</b></u> <br>
+        QMessageBox.about(self, "About", """     Welcome to Simple Rtl-Sdr Software Made by <u><b>Mehul Sutariya</b></u> <br>
                                                         For <i>Radio Astronomy Project</i> for<br> 
                                                         <b>Sardar Vallabhbhai National Institute of Technology</b>,Surat <br>
                                                         <b><i> Aim:Detection of 21cm Hydrogen Line</i></b>
@@ -317,20 +361,20 @@ class ApplicationWindow(QtWidgets.QMainWindow):                 # main gui windo
                                                         Thanks to Ankit Virani""")
 
     def infozoom(self):           # information of Zoom
-        QtWidgets.QMessageBox.about(self, "Zoom",""" Divide X-Axes into total 6 Part Which start From 0
+        QMessageBox.about(self, "Zoom",""" Divide X-Axes into total 6 Part Which start From 0
         (X-axes has total 6 part) and than Enter 
         The Start and End value and press Zoom button
           for REMOVE zoom press RESET""")
 
     def infoOverLap(self):                    # information of the overlap
-        QtWidgets.QMessageBox.about(self, "Over Lap", """ Total Over Laping in 2 Power Selected_Number 
-        i.e : Slected_Number = 5 Then OverLaping is 32 Time """)
+        QMessageBox.about(self, "Over Lap", """ Total Over Lapping in 2 Power Selected_Number 
+        i.e : Selected_Number = 5 Then OverLapping is 32 Time """)
 
     def changePosition(self):                    # show coordinate
         self.pos.setText(str(coordinate))
 
 
-qApp = QtWidgets.QApplication(sys.argv)
+qApp = QApplication(sys.argv)
 
 aw = ApplicationWindow()
 aw.setWindowTitle('Radio Astronomy Project')
